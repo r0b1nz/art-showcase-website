5f01e33d53cd2912d4b930ab745f57a3f2887b10
Robin Gautam <robin.gautam341@gmail.com>
2021-08-05 02:01:09 +0530

    update: correct page title

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'Chit1 Studios',
+  name: 'Chit1',
   shortName: 'Chit1',
-  description: 'A bold, minimal display of artifacts, focused on clean intentions & beautiful imagery',
+  description: 'A bold, minimal display of art, focused on clean intentions & beautiful imagery',
   url: 'https://artshowcasewebsite.gatsbyjs.io/',
   prefix: '/',
   author: 'Robin Gautam',
